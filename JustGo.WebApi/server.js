002f8f9870b78aa5755e61011c6e61b12a82563b
Pavel Dobranov <pdobranov@gmail.com>
2015-08-30 21:42:50 +0300

    Added express configuration

--- JustGo.WebApi/server.js
+++ JustGo.WebApi/server.js
@@ -6,6 +6,8 @@ var app = express();
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config/config')[env];
 
+require('./config/express');
+
 app.listen(config.port, function() {
   console.log('Server running on port: ' + config.port);
   console.log('Enviroment: ' + env);

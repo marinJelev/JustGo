f6518d2301db8ec314a231344e4a66c442d55614
ykomitov <yavork@gmail.com>
2015-08-30 13:29:05 +0300

    Added tripsController to config.js

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  System.config({
-    baseURL: '/app',
-    transpiler: 'babel',
-    map: {
-      babel: '../node_modules/babel-core/browser.js',
-      jquery: '../bower_components/jquery/dist/jquery.js',
-      jqueryRoutes: '../bower_components/jquery-routes/jquery.routes.js',
-      routie: '../bower_components/routie/dist/routie.js'
-    }
-  });
+    System.config({
+        baseURL: '/app',
+        transpiler: 'babel',
+        map: {
+            babel: '../node_modules/babel-core/browser.js',
+            jquery: '../bower_components/jquery/dist/jquery.js',
+            jqueryRoutes: '../bower_components/jquery-routes/jquery.routes.js',
+            routie: '../bower_components/routie/dist/routie.js',
+            tripsController: '../app/controllers/tripsController.js'
+        }
+    });
 }());

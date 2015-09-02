e7167cf7f82725cbccf3b10a59d9e88a5a1fcffa
ykomitov <yavork@gmail.com>
2015-09-02 08:54:33 +0300

    Initial work on My Places view

--- JustGo.WebClient/app/controllers/tripsController.js
+++ JustGo.WebClient/app/controllers/tripsController.js
@@ -30,27 +30,3 @@ function visualizeUserTripsData(tripsData){
 }
 
 export default {init};
-
-
-//$.ajax({
-//    type: 'GET',
-//    url: '../app/data/sampleTrips.json',
-//    data: '',
-//    dataType: 'json',
-//    success: function (ajaxData) {
-//
-//        $.get('app/views/tripsView.html', function (templateData) {
-//            var tripsTemplate = Handlebars.compile(templateData);
-//            $('#main-content').html(tripsTemplate(ajaxData));
-//        });
-//
-//        $('#main-content').on("click", "button", function (ev) {
-//            var buttonId = '#' + ev.target.id.split('-')[1];
-//
-//            $(buttonId).toggle("slow");
-//        });
-//    },
-//    error: function () {
-//        console.log('Error in ajax request!');
-//    }
-//});

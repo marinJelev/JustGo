e8fca2468c08c2426fc1837193b6fd2d4b7eccc3
unknown <on.tomas@gmail.com>
2015-09-02 19:51:50 +0300

    tripController refactoring

--- JustGo.WebClient/app/controllers/tripsController.js
+++ JustGo.WebClient/app/controllers/tripsController.js
@@ -1,35 +1,30 @@
 import httpRequester from '../utils/http-requester.js';
 import Handlebars from '../../bower_components/handlebars/handlebars.js';
+import templateGenerator from 'utils/templateGenerator.js';
 
 var TRIPS_URL = '../app/data/sampleTrips.json';
 var TRIPS_TEMPLATE = 'app/templates/trips.handlebars';
-var TRIPS_VIEW = 'app/views/tripsView.html';
 var $TEMPLATE_TARGET = $('#main-content');
+var trips = [];
+var $tripsContainer;
 
 function init() {
-
-    var promise = new Promise(function (resolve, reject) {
-        httpRequester
-            .get(TRIPS_URL, ' ')
-            .then(function (data) {
-                visualizeUserTripsData(data);
-                resolve();
-            });
-    });
-
-    return promise;
+    $('#main-content').load('app/views/tripsView.html', bindEvents);
 }
 
-function visualizeUserTripsData(tripsData) {
-    $.get(TRIPS_TEMPLATE, function (templateData) {
-        var tripsTemplate = Handlebars.compile(templateData);
-        var promise = new Promise(function (resolve, reject) {
-            $.get(TRIPS_VIEW, function (data) {
-                $TEMPLATE_TARGET.html(data + tripsTemplate(tripsData));
-            });
-            resolve();
+function bindEvents() {
+    $tripsContainer = $('#trips-container');
+    httpRequester
+        .get(TRIPS_URL)
+        .then(function (data) {
+            trips = data;
+            templateGenerator
+                .get(TRIPS_TEMPLATE)
+                .then(function (template) {
+                    $tripsContainer.html(template(data));
+                });
+
         });
-    });
 }
 
 $TEMPLATE_TARGET.on("click", "button", function (ev) {
